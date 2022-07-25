8da511e52a279f2b3d638dedce56f3e4dd09c47e
san9w9n <sw994812@hanyang.ac.kr>
2022-07-25 08:53:43 +0000

    Fix: chunk function

--- src/tasks/ppdb/ppdb.repository.js
+++ src/tasks/ppdb/ppdb.repository.js
@@ -13,60 +13,67 @@ class PpdbRepository {
     }
   }
 
-  static async #splitArrayIntoEqualChunks(arr, chunkSize) {
-    let firstIdx = 0;
+  static async #splitArrayIntoEqualChunks(arr) {
+    let chunkSize = 10;
+    if (chunkSize < 2) return [arr];
+
+    const arrayLength = arr.length;
     const chunkedArr = [];
-    for (let i = 0; i < arr.length; i += 1) {
-      if ((i + 1) % chunkSize === 0) {
-        const innerArr = [];
-        for (let j = firstIdx; j <= i; j += 1) {
-          innerArr.push(arr[j]);
-          firstIdx = i + 1;
-        }
-        chunkedArr.push(innerArr);
+    let i = 0;
+    if (arrayLength % chunkSize === 0) {
+      const size = Math.floor(arrayLength / chunkSize);
+      while (i < arrayLength) {
+        chunkedArr.push(arr.slice(i, i + size));
+        i += size;
+      }
+    } else {
+      while (i < arrayLength) {
+        const size = Math.ceil((arrayLength - i) / chunkSize);
+        chunkSize -= 1;
+        chunkedArr.push(arr.slice(i, i + size));
+        i += size;
       }
     }
     return chunkedArr;
   }
 
-  // static async savePpdbModelsOnDB(ppdbModelArray) {
-  //   try {
-  //     await this.#deleteAllPpdb();
-
-  //     const chunkedPpdbModelArray = await this.#splitArrayIntoEqualChunks(
-  //       ppdbModelArray,
-  //       10
-  //     );
-  //     const resultArray = await Promise.all(
-  //       chunkedPpdbModelArray.map(async (ppdbArray) => {
-  //         console.log(ppdbArray);
-  //         const res = await PpdbModel.insertMany(ppdbArray);
-  //         return res ? res.length : 0;
-  //       })
-  //     );
-  //     const insertedCount = resultArray.reduce((sum, e) => sum + e, 0);
-  //     console.log(insertedCount);
-  //     console.log(ppdbModelArray.length);
-  //     if (insertedCount !== ppdbModelArray.length) {
-  //       await this.#deleteAllPpdb();
-  //       throw new Error('Save ppdb failed.');
-  //     }
-  //   } catch (err) {
-  //     console.log(err);
-  //   }
-  // }
-
   static async savePpdbModelsOnDB(ppdbModelArray) {
     try {
       await this.#deleteAllPpdb();
-      await PpdbModel.insertMany(ppdbModelArray);
-      return PpdbModel.createIndexes({ tcaTime: 1, probability: 1, dca: 1 });
+
+      const chunkedPpdbModelArray = await this.#splitArrayIntoEqualChunks(
+        ppdbModelArray
+      );
+      const resultArray = await Promise.all(
+        chunkedPpdbModelArray.map(async (ppdbArray) => {
+          console.log(ppdbArray);
+          const res = await PpdbModel.insertMany(ppdbArray);
+          return res ? res.length : 0;
+        })
+      );
+      const insertedCount = resultArray.reduce((sum, e) => sum + e, 0);
+      console.log(insertedCount);
+      console.log(ppdbModelArray.length);
+      if (insertedCount !== ppdbModelArray.length) {
+        await this.#deleteAllPpdb();
+        throw new Error('Save ppdb failed.');
+      }
     } catch (err) {
       console.log(err);
-      await this.#deleteAllPpdb();
-      throw new Error('Save ppdb failed.');
     }
   }
+
+  // static async savePpdbModelsOnDB(ppdbModelArray) {
+  //   try {
+  //     await this.#deleteAllPpdb();
+  //     await PpdbModel.insertMany(ppdbModelArray);
+  //     return PpdbModel.createIndexes({ tcaTime: 1, probability: 1, dca: 1 });
+  //   } catch (err) {
+  //     console.log(err);
+  //     await this.#deleteAllPpdb();
+  //     throw new Error('Save ppdb failed.');
+  //   }
+  // }
 }
 
 module.exports = PpdbRepository;
