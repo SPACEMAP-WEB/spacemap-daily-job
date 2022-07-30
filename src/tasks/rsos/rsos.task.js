33a7246003acc4cd8d8f8f218431519c49cccda8
san9w9n <sw994812@hanyang.ac.kr>
2022-07-30 05:29:48 +0000

    Check: daily job

--- src/tasks/rsos/rsos.task.js
+++ src/tasks/rsos/rsos.task.js
@@ -43,19 +43,22 @@ class RsosTask {
         `${this.#SPACETRACK_URI}/${this.#AUTH_URI}`,
         process.env.SPACETRACK,
       );
-
+      console.log('RSO #1');
       // 2. get plain texts from spacetrack.
       const rsoParamsPlainText = await httpRequestHandler.getContentsRequest(
         `${this.#SPACETRACK_URI}/${this.#QUERY_URI}`,
         loginCookie,
       );
 
+      console.log('RSO #2');
       // 3. parse tleplaintexts
       const rsoJson = RsoHandler.parseRsoXml(rsoParamsPlainText);
       const rsoParamsArray = RsoHandler.getRsoParamArrays(rsoJson);
 
+      console.log('RSO #3');
       // 4. update ID-NAME pairs
       await RsosRepository.updateRsoParams(rsoParamsArray);
+      console.log('RSO #4');
     } catch (err) {
       console.log(err);
       if (MODE !== 'TEST') {
--- src/tasks/tasks-schedules.js
+++ src/tasks/tasks-schedules.js
@@ -1,10 +1,10 @@
 const lcaFrequency = '*/5 * * * * *';
 const watcherCatcherFrequency = '*/5 * * * * *';
 const collisionAvoidanceFrequency = '*/5 * * * * *';
-const tlesFrequency = '0 35 12 * * *';
-const rsosFrequency = '0 40 12 * * *';
-const eventSeqFrequency = '0 45 12 * * *';
-const ppdbFrequency = '0 0 19 * * *';
+const tlesFrequency = '0 30 5,11 * * *';
+const eventSeqFrequency = '0 35 5,11 * * *';
+const ppdbFrequency = '0 0 11,17 * * *';
+const rsosFrequency = '0 5 11,17 * * *';
 module.exports = {
   lcaFrequency,
   watcherCatcherFrequency,
